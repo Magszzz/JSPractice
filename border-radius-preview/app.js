6df0ccb9fd8aee6cce03387b4f4001cdb0215ac0
Magno Ranier <benitua.magno@gmail.com>
2020-08-01 12:59:08 +0000

    new changes

--- border-radius-preview/app.js
+++ border-radius-preview/app.js
@@ -1,23 +1,39 @@
-document.querySelector('#top-left-radius').addEventListener('input', () => {
-    const topLeft = document.querySelector('#top-left-radius').value;
-    const box = document.querySelector('#box');
-    box.style.borderTopLeftRadius = `${topLeft}px`;
+const tlr = document.querySelector('#top-left-radius');
+const trr = document.querySelector('#top-right-radius');
+const blr = document.querySelector('#bottom-left-radius');
+const brr = document.querySelector('#bottom-right-radius');
+const check = document.querySelector('#css-code');
+
+const allInput = document.querySelectorAll('.rad-value');
+
+allInput.forEach(el => {
+    el.addEventListener('input', borderChange);
+    check.addEventListener('click', () => {
+        check.classList.toggle('oncheck');
+        if(check.classList.contains('oncheck')){
+            el.addEventListener('input', textCode)
+        }
+    })
 })
 
-document.querySelector('#top-right-radius').addEventListener('input', () => {
-    const topRight = document.querySelector('#top-right-radius').value;
-    const box = document.querySelector('#box');
-    box.style.borderTopRightRadius = `${topRight}px`;
-})
 
-document.querySelector('#bottom-left-radius').addEventListener('input', () => {
-    const bottomLeft = document.querySelector('#bottom-left-radius').value;
+function borderChange(){
     const box = document.querySelector('#box');
-    box.style.borderBottomLeftRadius = `${bottomLeft}px`;
-})
+    
+    box.style.borderTopLeftRadius = `${tlr.value}`;
+    box.style.borderTopRightRadius = `${trr.value}`;
+    box.style.borderBottomLeftRadius = `${blr.value}`;
+    box.style.borderBottomRightRadius = `${brr.value}`;
+}
 
-document.querySelector('#bottom-right-radius').addEventListener('input', () => {
-    const bottomRight = document.querySelector('#bottom-right-radius').value;
-    const box = document.querySelector('#box');
-    box.style.borderBottomRightRadius = `${bottomRight}px`;
-})
+function textCode(){
+    if(tlr.value === trr.value && tlr.value === blr.value && tlr.value === brr.value){
+        console.log('same');
+    }else if(tlr.value === trr.value){
+        console.log('top radius are same');
+    }else if(blr.value === brr.value){
+        console.log('bottom radius are same');
+    }else{
+        console.log('not same');
+    }
+}
