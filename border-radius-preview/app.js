dee67c52e1bbb661a4e90e9b5c57df65562ed87d
Magno Ranier <benitua.magno@gmail.com>
2020-08-02 09:13:54 +0000

    final changes

--- border-radius-preview/app.js
+++ border-radius-preview/app.js
@@ -3,17 +3,13 @@ const trr = document.querySelector('#top-right-radius');
 const blr = document.querySelector('#bottom-left-radius');
 const brr = document.querySelector('#bottom-right-radius');
 const check = document.querySelector('#css-code');
-
+const div =  document.querySelector('.text-container');
+const section = div.querySelector('section');
 const allInput = document.querySelectorAll('.rad-value');
 
 allInput.forEach(el => {
     el.addEventListener('input', borderChange);
-    check.addEventListener('click', () => {
-        check.classList.toggle('oncheck');
-        if(check.classList.contains('oncheck')){
-            el.addEventListener('input', textCode)
-        }
-    })
+    el.addEventListener('input', textCode)
 })
 
 
@@ -28,12 +24,22 @@ function borderChange(){
 
 function textCode(){
     if(tlr.value === trr.value && tlr.value === blr.value && tlr.value === brr.value){
-        console.log('same');
-    }else if(tlr.value === trr.value){
-        console.log('top radius are same');
-    }else if(blr.value === brr.value){
-        console.log('bottom radius are same');
+        section.innerHTML = `border-radius: ${tlr.value}`;
+    }else if(tlr.value === trr.value && blr.value === brr.value){
+        section.innerHTML = `border-top-radius: ${tlr.value} <br>
+        border-bottom-radius: ${brr.value}
+        `;
+    }else if(tlr.value !== trr.value && blr.value === brr.value){
+        section.innerHTML =`border-top-left-radius: ${tlr.value} <br> border-top-right-radius: ${trr.value} <br>
+        border-bottom-radius: ${brr.value}`;
+    }else if(blr.value !== brr.value && tlr.value === trr.value){
+        section.innerHTML =`border-bottom-left-radius: ${blr.value} <br> border-bottom-right-radius: ${brr.value} <br>
+        border-top-radius: ${trr.value}`;
     }else{
-        console.log('not same');
+        section.innerHTML = `border-top-left-radius: ${tlr.value} <br> border-top-right-radius: ${trr.value} <br> border-bottom-left-radius: ${blr.value} <br> border-bottom-right-radius: ${brr.value} <br>`;
     }
 }
+
+check.addEventListener('click', () => {
+    section.classList.toggle('oncheck');
+})
